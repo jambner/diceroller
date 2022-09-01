28c3a7141017e1e0ff06ac2ebf3ad442cc1424f3
jambo <jambosucks@gmail.com>
2022-09-01 14:53:39 -0500

    Made button functional and generate random number

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +13,17 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's Roll"
         rollButton.setOnClickListener{
-            Toast.makeText(this, "ROLLING...", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Please Wait...", Toast.LENGTH_SHORT).show()
         }
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val randomInt = java.util.Random().nextInt(6) + 1
+
+        resultText.text= randomInt.toString()
     }
 }
